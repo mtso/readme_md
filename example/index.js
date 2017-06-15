d3e83764492217c8ebe9e155108ac3c80f88de22
mtso <mtso@tuta.io>
2017-06-14 21:19:50 -0700

    Add default properties readme/markup and change export to function object

--- example/index.js
+++ example/index.js
@@ -1,9 +1,9 @@
-const markup = require('readme_md');
+const readme = require('readme_md');
 const http = require('http');
 
 const server = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(markup);
+  res.write(readme.markup);
   res.end();
 });
 
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const fs = require('fs')
-const marked = require('marked')
+const fs = require('fs');
+const marked = require('marked');
 
 /**
- * Parses README.md file and returns HTML markup.
+ * Parses a markdown file and returns HTML markup.
  * @param {string?} path - Filepath to README.md.
  * @returns {string} markup - HTML markup string.
  */
-const markup = (function() {
-  const path = './README.md'
-  const raw = fs.readFileSync(path).toString()
-  return marked(raw)
-})()
+const render = function(path) {
+  path = path || './README.md';
+  const raw = fs.readFileSync(path).toString();
+  return marked(raw);
+};
 
-module.exports = markup
+render.markup = render.readme = render('./README.md');
+
+module.exports = render;
